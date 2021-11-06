a4cad719f6b50b46a3625659b62869cfd7441483
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-11-05 22:56:12 -0300

    Requisito - 6

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
       cardRare: 'normal',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
+      savedCards: [],
     };
     this.checkInputsValues = this.checkInputsValues.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
@@ -36,8 +37,36 @@ class App extends React.Component {
       })));
   }
 
-  onSaveButtonClick() {
-    // console.log('teste');
+  onSaveButtonClick(event) {
+    event.preventDefault();
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    this.setState((state) => ({ savedCards: [...state.savedCards, {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    }] }), () => this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    }));
   }
 
   // Checa se os valores dos 3 atributos estão corretos.
@@ -75,11 +104,11 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form
-          onInputChange={ this.onInputChange }
           { ... this.state }
+          onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
-        <Card { ... this.state } />
+        <Card { ... this.state } testePropsSeila="não sei" />
       </div>
     );
   }
